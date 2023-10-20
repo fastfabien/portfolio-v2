b6dc35f27c712b4b63ddf374aefbafc2a97d49fe
fastfabien <fastfabien56@gmail.com>
2023-10-20 09:52:24 +0300

    modif

--- src/Components/Left.jsx
+++ src/Components/Left.jsx
@@ -12,34 +12,40 @@ import { Linkedin, gitHub, gmail } from "../assets/SVG";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
-const Left = () => {
-  gsap.registerPlugin(ScrollTrigger);
-  const leftRef = useRef(null);
-  const [isActive, setIsActive] = useState("about");
+gsap.registerPlugin(ScrollTrigger);
 
-  const setActiveOnProgress = (s) => {
-    s.forEach((section) => {
+const Left = () => {
+  const leftRef = useRef(null);
+  const [isActive, setIsActive] = useState("");
+
+  useEffect(() => {
+    const sections = ["about", "experiences", "projects"];
+    sections.forEach((section) => {
       ScrollTrigger.create({
         trigger: `#${section}`,
         start: "top 20%",
         end: "bottom 40%",
         onEnter: () => {
-          setIsActive(section);
+          setIsActive(() => section);
         },
       });
     });
+
+    return () => {};
+  });
+
+  const debounce = (func, wait) => {
+    let timeout;
+    return function (...args) {
+      const context = this;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func.apply(context, args), wait);
+    };
   };
 
-  useEffect(() => {
-    const sections = ["about", "experiences", "projects"];
-    setActiveOnProgress(sections);
-  }, [isActive]);
-
-  const HandleScrollToExperiences = (id) => {
+  const handleScrollToExperiences = debounce((id) => {
     const about = document.getElementById("about");
     const element = document.getElementById(id);
-    console.log(about.offsetHeight);
-
     if (about && element) {
       let elementIn = about.offsetTop + about.offsetHeight / 2;
 
@@ -48,19 +54,24 @@ const Left = () => {
         behavior: "smooth",
       });
     }
-  };
+  }, 100);
 
-  const HandleScrollTo = (id) => {
+  const handleScrollTo = debounce((id) => {
     const element = document.getElementById(id);
-    let elementIn =
-      element.offsetTop - element.offsetHeight + element.offsetHeight / 2.5;
-
     if (element) {
+      let elementIn =
+        element.offsetTop - element.offsetHeight + element.offsetHeight / 2.5;
+
       window.scrollTo({
         top: elementIn,
         behavior: "smooth",
       });
     }
+  }, 100);
+
+  const handleClick = (e, id, scrollHandler) => {
+    e.preventDefault();
+    scrollHandler(id);
   };
 
   return (
@@ -71,21 +82,23 @@ const Left = () => {
       </Heading>
       <FooterNav>
         <LienNav
-          onClick={() => HandleScrollTo("about")}
+          onClick={(e) => handleClick(e, "about", handleScrollTo)}
           data-section="about"
           active={isActive === "about"}
         >
           a propos
         </LienNav>
         <LienNav
-          onClick={() => HandleScrollToExperiences("experiences")}
+          onClick={(e) =>
+            handleClick(e, "experiences", handleScrollToExperiences)
+          }
           data-section="experiences"
           active={isActive === "experiences"}
         >
           expériences
         </LienNav>
         <LienNav
-          onClick={() => HandleScrollTo("projects")}
+          onClick={(e) => handleClick(e, "projects", handleScrollTo)}
           data-section="projects"
           active={isActive === "projects"}
         >
